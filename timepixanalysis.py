f4f29b045cd403f34fd1809758ed37bf0e4fb159
lcalvin02 <lcalvin02@qub.ac.uk>
2024-01-10 15:17:25 +0000

    file created

---
+++ timepixanalysis.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.special import kn
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import math
+import matplotlib.ticker
+from matplotlib.ticker import (MultipleLocator, 
+                               FormatStrFormatter, 
+                               AutoMinorLocator) 
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+
+
